a6d3466af1f1318ca56693f23578185a2821729c
John Mitnick <overkazaf@gmail.com>
2015-12-25 23:52:40 +0800

    fix bug

--- hijack/background.js
+++ hijack/background.js
@@ -1,8 +1,18 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-    	console.log(details);
         return {
-            cancel: details.url.indexOf("://mail.trueway.com.cn/") != -1
+            //cancel: details.url.indexOf("://mail.trueway.com.cn/") != -1
+        };
+    }, {
+        urls: ["<all_urls>"]
+    }, ["blocking"]);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+    	console.log('headers');
+    	console.log(details.requestHeaders);
+        return {
+            //cancel: details.url.indexOf("://mail.trueway.com.cn/") != -1
         };
     }, {
         urls: ["<all_urls>"]
